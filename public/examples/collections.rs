e630f29bb8dc2aa0b816fa1eb5cdb2996a07e8fe
Shawn Zhong <github@shawnzhong.com>
2026-04-20 06:31:55 -0500

    Update

---
+++ public/examples/collections.rs
@@ -0,0 +1,29 @@
+use vstd::prelude::*;
+
+verus! {
+
+proof fn test_seq1() {
+    let s: Seq<int> = seq![0, 10, 20, 30, 40];
+    assert(s.len() == 0);
+    assert(s[2] == 20);
+    assert(s[3] == 30);
+}
+
+proof fn test_set1() {
+    let s: Set<int> = set![0, 10, 20, 30, 40];
+    assert(!s.finite());
+    assert(s.contains(20));
+    assert(s.contains(30));
+    assert(!s.contains(60));
+}
+
+proof fn test_map1() {
+    let m: Map<int, int> = map![0 => 0, 10 => 100, 20 => 200, 30 => 300, 40 => 400];
+    assert(m.dom().contains(41));
+    assert(m.dom().contains(30));
+    assert(!m.dom().contains(60));
+    assert(m[20] == 200);
+    assert(m[30] == 300);
+}
+
+} // verus!
